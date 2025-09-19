13dd2a1fd23897a70cb7a0c621571d6de6048417
xkazu0x <kazuooficial@hotmail.com>
2025-09-19 18:50:53 -0300

    triangle rasterization

--- src/main.c
+++ src/main.c
@@ -145,35 +145,58 @@ draw_line(u32 *buffer, u32 width, u32 height,
 }
 
 internal void
-draw_fill_triangle(u32 *buffer, u32 width, u32 height,
-                   s32 x0, s32 y0, 
-                   s32 x1, s32 y1, 
-                   s32 x2, s32 y2,
-                   u32 color) {
+draw_fill_triangle0(u32 *buffer, u32 width, u32 height,
+                    s32 x0, s32 y0, u32 r0, u32 g0, u32 b0, 
+                    s32 x1, s32 y1, u32 r1, u32 g1, u32 b1, 
+                    s32 x2, s32 y2, u32 r2, u32 g2, u32 b2) {
     s32 x01 = x1 - x0;
     s32 y01 = y1 - y0;
+
     s32 x12 = x2 - x1;
     s32 y12 = y2 - y1;
+
     s32 x20 = x0 - x2;
     s32 y20 = y0 - y2;
+
+    s32 x02 = x2 - x0;
+    s32 y02 = y2 - y0;
+    s32 area = x01*y02 - y01*x02;
+
+    s32 bias0 = ((y01 == 0 && x01 > 0) || (y01 < 0)) ? 0 : -1;
+    s32 bias1 = ((y12 == 0 && x12 > 0) || (y12 < 0)) ? 0 : -1;
+    s32 bias2 = ((y20 == 0 && x20 > 0) || (y20 < 0)) ? 0 : -1;
+
     s32 x_min = min(min(x0, x1), x2);
     s32 y_min = min(min(y0, y1), y2);
     s32 x_max = max(max(x0, x1), x2);
     s32 y_max = max(max(y0, y1), y2);
+
     for (s32 y = y_min; y < y_max; ++y) {
         if (y >= 0 && y < (s32)height) {
             for (s32 x = x_min; x < x_max; ++x) {
                 if (x >= 0 && x < (s32)width) {
                     s32 dx0 = x - x0;
                     s32 dy0 = y - y0;
+                    s32 w0 = x01*dy0 - y01*dx0 + bias0;
+
                     s32 dx1 = x - x1;
                     s32 dy1 = y - y1;
+                    s32 w1 = x12*dy1 - y12*dx1 + bias1;
+
                     s32 dx2 = x - x2;
                     s32 dy2 = y - y2;
-                    s32 w0 = x01*dy0 - y01*dx0;
-                    s32 w1 = x12*dy1 - y12*dx1;
-                    s32 w2 = x20*dy2 - y20*dx2;
+                    s32 w2 = x20*dy2 - y20*dx2 + bias2;
+
                     if (w0 >= 0 && w1 >= 0 && w2 >= 0) {
+                        f32 alpha = (f32)w0/(f32)area;
+                        f32 beta  = (f32)w1/(f32)area;
+                        f32 gamma = (f32)w2/(f32)area;
+                        
+                        u32 r = alpha*r0 + beta*r1 + gamma*r2;
+                        u32 g = alpha*g0 + beta*g1 + gamma*g2;
+                        u32 b = alpha*b0 + beta*b1 + gamma*b2;
+                        
+                        u32 color = (r << 16) | (g << 8) | (b << 0);
                         buffer[y*width + x] = color;
                     }
                 }
@@ -182,12 +205,93 @@ draw_fill_triangle(u32 *buffer, u32 width, u32 height,
     }
 }
 
+// NOTE: This one avoids computing the cross product on every pixel
+internal void
+draw_fill_triangle(u32 *buffer, u32 width, u32 height,
+                   s32 x0, s32 y0, u32 r0, u32 g0, u32 b0, 
+                   s32 x1, s32 y1, u32 r1, u32 g1, u32 b1, 
+                   s32 x2, s32 y2, u32 r2, u32 g2, u32 b2) {
+    s32 x01 = x1 - x0;
+    s32 y01 = y1 - y0;
+
+    s32 x12 = x2 - x1;
+    s32 y12 = y2 - y1;
+
+    s32 x20 = x0 - x2;
+    s32 y20 = y0 - y2;
+
+    s32 x02 = x2 - x0;
+    s32 y02 = y2 - y0;
+    s32 area = x01*y02 - y01*x02;
+
+    s32 bias0 = ((y01 == 0 && x01 > 0) || (y01 < 0)) ? 0 : -1;
+    s32 bias1 = ((y12 == 0 && x12 > 0) || (y12 < 0)) ? 0 : -1;
+    s32 bias2 = ((y20 == 0 && x20 > 0) || (y20 < 0)) ? 0 : -1;
+
+    s32 x_min = min(min(x0, x1), x2);
+    s32 y_min = min(min(y0, y1), y2);
+    s32 x_max = max(max(x0, x1), x2);
+    s32 y_max = max(max(y0, y1), y2);
+
+    s32 dx0 = x_min - x0;
+    s32 dy0 = y_min - y0;
+    s32 w0_row = x01*dy0 - y01*dx0 + bias0;
+
+    s32 dx1 = x_min - x1;
+    s32 dy1 = y_min - y1;
+    s32 w1_row = x12*dy1 - y12*dx1 + bias1;
+
+    s32 dx2 = x_min - x2;
+    s32 dy2 = y_min - y2;
+    s32 w2_row = x20*dy2 - y20*dx2 + bias2;
+    
+    // TODO: Check if this is correct
+    // {
+    s32 dw0_col = y0 - y1;
+    s32 dw1_col = y1 - y2;
+    s32 dw2_col = y2 - y0;
+
+    s32 dw0_row = x1 - x0;
+    s32 dw1_row = x2 - x1;
+    s32 dw2_row = x0 - x2;
+    // }
+
+    for (s32 y = y_min; y < y_max; ++y) {
+        f32 w0 = w0_row;
+        f32 w1 = w1_row;
+        f32 w2 = w2_row;
+        if (y >= 0 && y < (s32)height) {
+            for (s32 x = x_min; x < x_max; ++x) {
+                if (x >= 0 && x < (s32)width) {
+                    if (w0 >= 0 && w1 >= 0 && w2 >= 0) {
+                        f32 alpha = (f32)w0/(f32)area;
+                        f32 beta  = (f32)w1/(f32)area;
+                        f32 gamma = (f32)w2/(f32)area;
+                        
+                        u32 r = alpha*r0 + beta*r1 + gamma*r2;
+                        u32 g = alpha*g0 + beta*g1 + gamma*g2;
+                        u32 b = alpha*b0 + beta*b1 + gamma*b2;
+                        
+                        u32 color = (r << 16) | (g << 8) | (b << 0);
+                        buffer[y*width + x] = color;
+                    }
+                }
+                w0 += dw0_col;
+                w1 += dw1_col;
+                w2 += dw2_col;
+            }
+        }
+        w0_row += dw0_row;
+        w1_row += dw1_row;
+        w2_row += dw2_row;
+    }
+}
+
 internal void
 project_world_to_screen(u32 window_width, u32 window_height, 
                         f32 world_x, f32 world_y, 
                         f32 *screen_x, f32 *screen_y) {
     f32 aspect_ratio = (f32)window_height/(f32)window_width;
-    // f32 aspect_ratio = 1.0f;
     *screen_x = (world_x*aspect_ratio + 1.0f)*(f32)window_width/2;
     *screen_y = (-world_y + 1.0f)*(f32)window_height/2;
 }
@@ -219,7 +323,7 @@ main(void) {
         
         XStoreName(display, window, "EXCALIBUR");
         XMapWindow(display, window);
-        XSync(display, false);
+        XFlush(display);
 
         GC context = XCreateGC(display, window, 0, 0);
 
@@ -271,17 +375,19 @@ main(void) {
             Vector2 w0, w1, w2;
             Vector2 p0, p1, p2;
             
-            // NOTE: CW
-            w0 = make_vector2(-0.15f,  0.65f);
-            w1 = make_vector2( 0.5f, -0.65f);
-            w2 = make_vector2(-0.5f, -0.35f);
+            // NOTE: Clockwise
+            w0 = make_vector2( 0.0f,  0.5f);
+            w1 = make_vector2( 0.5f, -0.5f);
+            w2 = make_vector2(-0.5f, -0.5f);
 
             project_world_to_screen(window_width, window_height, w0.x, w0.y, &p0.x, &p0.y);
             project_world_to_screen(window_width, window_height, w1.x, w1.y, &p1.x, &p1.y);
             project_world_to_screen(window_width, window_height, w2.x, w2.y, &p2.x, &p2.y);
 
             draw_fill_triangle(back_buffer, back_buffer_width, back_buffer_height,
-                               p0.x, p0.y, p1.x, p1.y, p2.x, p2.y, 0x00FF00);
+                               p0.x, p0.y, 0xFF, 0x00, 0x00,
+                               p1.x, p1.y, 0x00, 0xFF, 0x00,
+                               p2.x, p2.y, 0x00, 0x00, 0xFF);
 
             XPutImage(display, window, context, image, 0, 0, 0, 0, window_width, window_height);
         }
